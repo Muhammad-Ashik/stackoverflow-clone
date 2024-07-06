d2b1aad57a1cf1ad04cf69ac569b60fc3a98d09a
muhammad-ashik <ashik.chowdhury79@gmail.com>
2024-07-06 21:00:27 +0600

    fix: add parser options

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -5,7 +5,15 @@ import tseslint from "typescript-eslint";
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { languageOptions: { globals: globals.browser } },
+  {
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: "./tsconfig.json",
+    },
+  },
+  {
+    rules: {},
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
