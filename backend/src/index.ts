d805f6599056ea417765b8ce587251f0dd544680
muhammad-ashik <ashik.chowdhury79@gmail.com>
2024-07-07 00:00:24 +0600

    fix: separate database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,14 +3,14 @@ import AppDataSource from './config/databaseConfig';
 
 const PORT = process.env.SERVER_PORT || 5000;
 
+// Start your Express server after the DB connection is established
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 AppDataSource.initialize()
   .then(() => {
     console.log('Database has been initialized!');
-
-    // Start your Express server after the DB connection is established
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
   })
   .catch((err) => {
     console.error('Error during Data Source initialization:', err);
