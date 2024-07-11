592f67b97a666d68e2a40d1c8ecc1efc2d9b2d06
muhammad-ashik <ashik.chowdhury79@gmail.com>
2024-07-12 04:58:49 +0600

    fix: added some example in app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -28,4 +28,117 @@ app.get('/users', async (req: Request, res: Response) => {
   }
 });
 
+// write a route where if hit then it will create 50 users in the user table
+app.get('/create-users', async (req: Request, res: Response) => {
+  try {
+    const userRepo = AppDataSource.getRepository(User);
+    for (let i = 0; i < 50; i++) {
+      const user = new User();
+      user.username = `User${i}`;
+      user.email = `User@${i}`;
+      user.password = 'password';
+      await userRepo.save(user);
+    }
+    res.send('Users created successfully');
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// write a route where if hit then it will delete all the users from the user table
+app.get('/delete-users', async (req: Request, res: Response) => {
+  try {
+    const userRepo = AppDataSource.getRepository(User);
+    await userRepo.clear();
+    res.send('Users deleted successfully');
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// write a route where if hit then it will delete the user with id send in the query params
+app.get('/delete-user', async (req: Request, res: Response) => {
+  try {
+    const userRepo = AppDataSource.getRepository(User);
+    const user = await userRepo.findOne({
+      where: { id: Number(req.query.id) },
+    });
+    if (user) {
+      await userRepo.remove(user);
+      res.send('User deleted successfully');
+    } else {
+      res.send('User not found');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+// write a route where if hit then it will create a user in the user table
+app.get('/create-user', async (req: Request, res: Response) => {
+  try {
+    const userRepo = AppDataSource.getRepository(User);
+    const user = new User();
+    user.username = 'New User';
+    user.email = 'User@new';
+    user.password = 'password';
+    await userRepo.save(user);
+    res.send('User created successfully');
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// write a route where if hit then it will get the user with id send in the query params
+app.get('/get-user', async (req: Request, res: Response) => {
+  try {
+    const userRepo = AppDataSource.getRepository(User);
+    const user = await userRepo.findOne({
+      where: { id: Number(req.query.id) },
+    });
+    if (user) {
+      res.json(user);
+    } else {
+      res.send('User not found');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// write a route where if hit then it will get the user with id from the url like /get-user/2
+app.get('/get-user/:id', async (req: Request, res: Response) => {
+  try {
+    const userRepo = AppDataSource.getRepository(User);
+    const user = await userRepo.findOne({
+      where: { id: Number(req.params.id) },
+    });
+    if (user) {
+      res.json(user);
+    } else {
+      res.send('User not found');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// write a route where if hit then it will update the user with id 2
+app.get('/update-user', async (req: Request, res: Response) => {
+  try {
+    const userRepo = AppDataSource.getRepository(User);
+    const user = await userRepo.findOne({
+      where: { id: Number(req.query.id) },
+    });
+    if (user) {
+      user.username = 'Updated User';
+      await userRepo.save(user);
+      res.send('User updated successfully');
+    } else {
+      res.send('User not found');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 export default app;
